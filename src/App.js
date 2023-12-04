eb8248d356a6d2142ee2b7936ce6685e6ef23c41
dannyhma <dannyhimawan089@gmail.com>
2023-12-05 06:24:51 +0700

    Soc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Welcomeh2, Welcomeh3 } from "./Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcomeh2 children="Welcome 1" />
+      <Welcomeh3 />
+      <Welcomeh2>Saya adalah content</Welcomeh2>
     </div>
   );
 }
