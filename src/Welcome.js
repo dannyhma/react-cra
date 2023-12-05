bbac353f70b6e51d800f7f1223fa3ea7cf805880
dannyhma <dannyhimawan089@gmail.com>
2023-12-05 23:09:52 +0700

    styling

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,8 @@
 export function Welcomeh2(props) {
   console.log(props);
-  return <h2>{props.children}</h2>;
+  return <h2 className="h2">{props.children}</h2>;
 }
 
 export function Welcomeh3() {
-  return <h3>Welcome h3</h3>;
+  return <h3 className="h3">Welcome h3</h3>;
 }
