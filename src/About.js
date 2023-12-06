a45568dcd5c461a06d9fd94772be9724cea6cd91
dannyhma <dannyhimawan089@gmail.com>
2023-12-06 11:03:59 +0700

    nested route

--- src/About.js
+++ src/About.js
@@ -1,3 +1,18 @@
+import Button from "./Button";
+import { Link, Outlet } from "react-router-dom";
+
 export default function About() {
-  return <h1>Halaman About</h1>;
+  return (
+    <>
+      <h1>Halaman About</h1>
+      <p>
+        Ini adalah halaman about, untuk info lainnya anda bisa click link
+        dibawah ini
+      </p>
+      <Link to="/about/team">
+        <Button>Click Me!</Button>
+      </Link>
+      <Outlet />
+    </>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import AboutTeam from "./AboutTeam";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="about" element={<About />}>
+          <Route path="team" element={<AboutTeam />} />
+        </Route>
       </Routes>
     </div>
   );
